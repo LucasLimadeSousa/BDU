4b8d1dc6a5f3c72c576518e5acbbdb7a8df173c0
L1MA <95104431+LucasLimadeSousa@users.noreply.github.com>
2026-04-22 12:23:47 -0300

    Junção das telas
    As telas de todas as branchs foram adicionadas na branch principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

16fc780e7a35f1c2ebdc0666d26b3419e8bb3b26
Lucas <lucaslima2020v2@gmail.com>
2026-05-03 17:36:22 -0300

    Mudanças joão

--- app/src/main/java/com/example/bdu/livros/ListadeEsperaActivity.kt
+++ app/src/main/java/com/example/bdu/livros/ListadeEsperaActivity.kt
@@ -1,21 +1,59 @@
 package com.example.bdu.livros
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bdu.R
+import com.example.bdu.usuario.MeuPerfilActivity
 
 class ListadeEsperaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val btnFila = findViewById<ImageButton?>(R.id.btn_nav_fila)
+        val btnMeusLivros = findViewById<ImageButton?>(R.id.btn_nav_meuslivros)
+        val btnHome = findViewById<ImageButton?>(R.id.btn_nav_home)
+        val btnDesejos = findViewById<ImageButton?>(R.id.btn_nav_desejos)
+        val btnperfil = findViewById<ImageButton?>(R.id.btn_nav_perfil)
+
         setContentView(R.layout.livros_lista_de_espera)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnFila?.setOnClickListener {
+            val intent = Intent(this, ListadeEsperaActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        btnMeusLivros?.setOnClickListener {
+            val intent = Intent(this, MeusLivrosActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnHome?.setOnClickListener {
+            val intent = Intent(this, TelahomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnDesejos?.setOnClickListener {
+            val intent = Intent(this, TelahomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnperfil?.setOnClickListener {
+            val intent = Intent(this, MeuPerfilActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/bdu/livros/VerTudoActivity.kt
+++ app/src/main/java/com/example/bdu/livros/VerTudoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bdu.livros
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +13,19 @@ class VerTudoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val btnreturn = findViewById<ImageButton?>(R.id.btnBackLivro)
+
         setContentView(R.layout.livros_ver_tudo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnreturn?.setOnClickListener {
+            val intent = Intent(this, TelahomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }

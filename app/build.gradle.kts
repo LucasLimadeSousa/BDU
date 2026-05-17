6e3b5f4706bd4b137c603d4bd15bde31f0482302
Joao <joaotravassos2345@gmail.com>
2026-05-17 12:37:59 -0300

    Coloquei a API da biblioteca

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,8 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-}
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    }

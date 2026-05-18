ae2b224b6cd865724a322aa6d00145323cece589
Lucas <lucaslima2020v2@gmail.com>
2026-05-18 17:34:22 -0300

    Correção bugs

---
+++ app/src/main/java/com/example/bdu/api/BooksApi.kt
@@ -0,0 +1,14 @@
+package com.example.bdu.api
+
+import com.example.bdu.model.BooksResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BooksApi {
+
+    @GET("volumes")
+    suspend fun searchBooks(
+        @Query("q") query: String,
+        @Query("key") apiKey: String? = null
+    ): BooksResponse
+}
